ee79c689452d53a980961ab7ff5864513676d0bc
Amrit Bera <a.bera@scopegroup.com>
2017-09-11 12:12:30 +0200

    Bit more informative

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var io = require('socket.io')(app);
 var fs = require('fs');
 
 app.listen(8080);
-console.log('Listening on 8080');
+console.log('Socket-io server running on 8080. ');
+console.log('Emit to "socketio-client" for debugging.');
 
 io.on('connection', function (socket) {
   socket.emit('message', { hello: 'world' });
   socket.on('socketio-client', function (data) {
-    console.log(data);
+    console.log('type: ', typeof(data), ' \ndata: ', data, '\n');
     socket.emit('socketio-client', data);
   });
 });
